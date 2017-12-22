f4abf733596468ae816671b6c56d59930f08a9a6
lipeng <lipeng-ds3@gomeplus.com>
2017-12-22 17:22:04 +0800

    init

---
+++ app/src/main/java/com/lipeng/mygithub/HomePageActivity.java
@@ -0,0 +1,17 @@
+package com.lipeng.mygithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * 主页面
+ * @author lipeng
+ * @date 2017/12/22
+ * */
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
